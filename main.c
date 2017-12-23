d7bda7abe0e4bb1a2abb90683b5a9ba71685b7d6
adri7101 <34715093+adri7101@users.noreply.github.com>
2017-12-23 15:20:00 +0100

    Version non finale

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
+//
+//  main.c
+//  Projet CALM
+//  VERSION NON FINALE
+//
+//  Created by Adrien MIGNEROT on 20/12/2017.
+//  Copyright © 2017 Projet CALM. All rights reserved.
+//
 
+#include "Header.h"
+
+joueur repertoire[REP];
+char tableau[nblignes][nbcolonnes];
+int i = 0;
+
+int main(int argc, const char * argv[]) {
+    printf("*** Bienvenue sur le jeu des morpions ! ***\n\n");
+    printf("Saisir le nom du premier joueur :\n");
+    scanf(" %s", repertoire[0].nom);
+    printf("Saisir le nom du second joueur :\n");
+    scanf(" %s", repertoire[1].nom);
+    
+    inittab(tableau); //initialise le jeu avec des cases vides
+    affichertab(tableau); //affiche le jeu (vide)
+    do
+    {
+        modiftab(tableau, repertoire, i);//enregistre la saisie du joueur (1 si i est paire, sinon 2).
+        i++;
+    }
+    while(!testgagnant(tableau, repertoire));//test si 3 mêmes cractères sont alignés, si oui, ajoute des points au score du gagnant et le félicite.
+    return 0;
+}
