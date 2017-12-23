966f29cb29216974b479c88828e7a81149de00a2
adri7101 <34715093+adri7101@users.noreply.github.com>
2017-12-23 15:20:43 +0100

    Version non finale

--- header.h
+++ header.h
@@ -1,1 +1,33 @@
+//
+//  main.c
+//  Projet CALM
+//  VERSION NON FINALE
+//
+//  Created by Adrien MIGNEROT on 20/12/2017.
+//  Copyright © 2017 Projet CALM. All rights reserved.
+//
 
+#include <stdio.h>
+#include <string.h>
+#include <MacTypes.h>
+
+#ifndef Source_h
+#define Source_h
+#define NOM ((int)100)
+#define REP ((int)50)
+#define nblignes 3
+#define nbcolonnes 3
+
+typedef struct //Joueur
+{
+    char nom[NOM];
+    int score;
+}joueur;
+
+joueur initJoueur(void);
+void inittab(char tableau[nblignes][nbcolonnes]);
+void modiftab(char tableau[nblignes][nbcolonnes], joueur repertoire[], int i);
+void affichertab(char tableau[nblignes][nbcolonnes]);
+int testgagnant(char tableau[nblignes][nbcolonnes], joueur repertoire[]);
+
+#endif /* Source_h */
