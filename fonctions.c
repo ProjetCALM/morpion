82c8f63f2e06c9832d156c65221fbb517a3fee42
adri7101 <34715093+adri7101@users.noreply.github.com>
2017-12-30 19:57:45 +0100

    SANS IA

--- fonctions.c
+++ fonctions.c
@@ -1,13 +1,14 @@
 //
 //  main.c
 //  Projet CALM
-//  VERSION NON FINALE
+//  VERSION SANS IA
 //
 //  Created by Adrien MIGNEROT on 20/12/2017.
 //  Copyright © 2017 Projet CALM. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <MacTypes.h>
@@ -20,7 +21,7 @@ joueur initJoueur(){
     return variable;
 }
 
-void inittab(char tableau[nblignes][nbcolonnes])
+void initTab(char tableau[nblignes][nbcolonnes])
 {
     int i = 0;
     int j = 0;
@@ -35,7 +36,7 @@ void inittab(char tableau[nblignes][nbcolonnes])
     
 }
 
-void modiftab(char tableau[nblignes][nbcolonnes], joueur repertoire[], int i)
+void modifTab(char tableau[nblignes][nbcolonnes], joueur repertoire[], int i)
 {
     
     int colonne;
@@ -46,23 +47,39 @@ void modiftab(char tableau[nblignes][nbcolonnes], joueur repertoire[], int i)
     scanf("%d", &ligne);
         printf("\nChoisir une colonne :\n");
     scanf("%d",&colonne);
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    tableau [ligne -1][colonne -1] ='X';
-    affichertab(tableau);
+    system("cls");
+    while(tableau [ligne -1][colonne -1] != ' '){
+        afficherTab(tableau);
+        printf("Cette case est deja prise !\n");
+        printf("Choisir une ligne :\n");
+        scanf("%d", &ligne);
+        printf("\nChoisir une colonne :\n");
+        scanf("%d",&colonne);
+    }
+        tableau [ligne -1][colonne -1] ='X';
+    afficherTab(tableau);
     } else {
     printf("Au tour de %s :\n\n", repertoire[1].nom);
         printf("Choisir une ligne :\n");
     scanf("%d", &ligne);
         printf("\nChoisir une colonne :\n");
     scanf("%d",&colonne);
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    tableau [ligne -1][colonne -1] ='O';
-    affichertab(tableau);
+    system("cls");
+    while(tableau [ligne -1][colonne -1] != ' '){
+        afficherTab(tableau);
+        printf("Cette case est deja prise !\n");
+        printf("Choisir une ligne :\n");
+        scanf("%d", &ligne);
+        printf("\nChoisir une colonne :\n");
+        scanf("%d",&colonne);
+    }
+        tableau [ligne -1][colonne -1] ='O';
+    afficherTab(tableau);
     }
     
 }
 
-void affichertab(char tableau[nblignes][nbcolonnes])
+void afficherTab(char tableau[nblignes][nbcolonnes])
 {
     printf("\n\n\n");
     printf("\t+-----------+\n");
@@ -77,13 +94,13 @@ void affichertab(char tableau[nblignes][nbcolonnes])
     
 }
 
-int testgagnant(char tableau[nblignes][nbcolonnes], joueur repertoire[])
+int testGagnant(char tableau[nblignes][nbcolonnes], joueur repertoire[])
 {
-    int i, j, somme, caseVide = 0;
+    int i, j, somme = 0, caseVide = 0;
     for (i=0; i<nblignes; i++, somme=0)
     {
         for (j=0; j<nbcolonnes; j++){
-            somme=somme+tableau[i][j];
+            somme+=tableau[i][j];
         }
         if(somme==264)
         {
@@ -140,4 +157,303 @@ int testgagnant(char tableau[nblignes][nbcolonnes], joueur repertoire[])
     }
                 printf("Aucun des 2 joueurs n'a gagne \n");
                 return 2;
+}
+
+int testGagnantTournoi(char tableau[nblignes][nbcolonnes], joueur repertoire[], int joueurA, int joueurB)
+{
+    int i, j, somme = 0, caseVide = 0;
+    char car;
+    for (i=0; i<nblignes; i++, somme=0)
+    {
+        for (j=0; j<nbcolonnes; j++){
+            somme+=tableau[i][j];
+        }
+        if(somme==264)
+        {
+            printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurA].nom);
+            repertoire[joueurA].pointsTournoi+=1;
+            printf("\n* Saisir un caractere pour continuer. *\n");
+            scanf(" %c", &car);
+            system("cls");
+            return 1;
+        }
+        if(somme==237)
+        {
+            printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurB].nom);
+            repertoire[joueurB].pointsTournoi+=1;
+            printf("\n* Saisir un caractere pour continuer. *\n");
+            scanf(" %c", &car);
+            system("cls");
+            return 1;
+        }
+    }
+    for(j=0 ; j<nbcolonnes ; j++, somme=0)
+    {
+        for(i=0 ; i<nblignes ; i++){
+            somme+=tableau[i][j] ;
+        }
+        if(somme==264)
+        {
+            printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurA].nom);
+            repertoire[joueurA].pointsTournoi+=1;
+            printf("\n* Saisir un caractere pour continuer. *\n");
+            scanf(" %c", &car);
+            system("cls");
+            return 1;
+        }
+        if(somme==237)
+        {
+            printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurB].nom);
+            repertoire[joueurB].pointsTournoi+=1;
+            printf("\n* Saisir un caractere pour continuer. *\n");
+            scanf(" %c", &car);
+            system("cls");
+            return 1;
+        }
+    }
+    if((tableau[0][0] + tableau[1][1] + tableau[2][2] == 264) ||
+       (tableau[0][2] + tableau[1][1] + tableau[2][0] == 264))
+    {
+        printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurA].nom);
+        repertoire[joueurA].pointsTournoi+=1;
+        printf("\n* Saisir un caractere pour continuer. *\n");
+        scanf(" %c", &car);
+        system("cls");
+        return 1;
+    }
+    if((tableau[0][0] + tableau[1][1] + tableau[2][2] == 237) ||
+       (tableau[0][2] + tableau[1][1] + tableau[2][0] == 237))
+    {
+        printf("Felicitations %s, tu remporte la partie !\n", repertoire[joueurB].nom);
+        repertoire[joueurB].pointsTournoi+=1;
+        printf("\n* Saisir un caractere pour continuer. *\n");
+        scanf(" %c", &car);
+        system("cls");
+        return 1;
+    }
+    /* Test si le tableau est plein :*/
+    for(i=0; i<nblignes; i++, caseVide = 0){
+        for(j=0; j<nbcolonnes; j++) {
+            if(tableau[i][j] == ' '){
+                return 0;
+            }
+        }
+    }
+    int chiffre = 0, tire = 0;
+    srand(time(NULL));
+    tire = rand()%1;
+    printf("Aucun des 2 joueurs n'a gagne \n");
+    printf("Procedons a un tirage au sort ! \n");
+    printf("%s doit choisir un chiffre entre 0 et 1 : \n", repertoire[joueurA].nom);
+    scanf(" %d", &chiffre);
+    while(chiffre != 0 && chiffre != 1){
+        printf("%s doit choisir un chiffre entre 0 et 1 : \n", repertoire[joueurA].nom);
+        scanf(" %d", &chiffre);
+    }
+    if(chiffre == tire){
+        printf("Felicitations %s, tu remporte le tirage !\n", repertoire[joueurA].nom);
+        repertoire[joueurA].pointsTournoi+=1;
+    } else {
+        printf("Felicitations %s, tu remporte le tirage !\n", repertoire[joueurB].nom);
+        repertoire[joueurB].pointsTournoi+=1;
+    }
+    printf("\n* Saisir un caractere pour continuer. *\n");
+    scanf(" %c", &car);
+    system("cls");
+    return 2;
+}
+
+void modifTabTournoi(char tableau[nblignes][nbcolonnes], joueur repertoire[], int i, int joueurA, int joueurB)
+{
+
+    int colonne;
+    int ligne;
+    if(i%2 == 0){
+        printf("Au tour de %s :\n\n", repertoire[joueurA].nom);
+        printf("Choisir une ligne :\n");
+        scanf("%d", &ligne);
+        printf("\nChoisir une colonne :\n");
+        scanf("%d",&colonne);
+        system("cls");
+        while(tableau [ligne -1][colonne -1] != ' '){
+        afficherTab(tableau);
+        printf("Cette case est deja prise !\n");
+        printf("Choisir une ligne :\n");
+        scanf("%d", &ligne);
+        printf("\nChoisir une colonne :\n");
+        scanf("%d",&colonne);
+        }
+        tableau [ligne -1][colonne -1] ='X';
+        afficherTab(tableau);
+    } else {
+        printf("Au tour de %s :\n\n", repertoire[joueurB].nom);
+        printf("Choisir une ligne :\n");
+        scanf("%d", &ligne);
+        printf("\nChoisir une colonne :\n");
+        scanf("%d",&colonne);
+        system("cls");
+        while(tableau [ligne -1][colonne -1] != ' '){
+            afficherTab(tableau);
+            printf("Cette case est deja prise !\n");
+            printf("Choisir une ligne :\n");
+            scanf("%d", &ligne);
+            printf("\nChoisir une colonne :\n");
+            scanf("%d",&colonne);
+        }
+        tableau [ligne -1][colonne -1] ='O';
+        afficherTab(tableau);
+    }
+    
+}
+
+void initPointsTournoi(joueur repertoire[]){
+    int i = 0;
+    for(i=0;i<8;i++){
+        repertoire[i].pointsTournoi=0;
+    }
+}
+
+void tournoi(char tableau[nblignes][nbcolonnes], joueur repertoire[], int nombreJoueurs)
+{
+    int i = 0;
+    system("cls");
+    printf("*** Vous etes en mode tournoi pour %d joueurs ***\n\n", nombreJoueurs);
+    switch(nombreJoueurs){
+        case 4 :
+            initPointsTournoi(repertoire);
+            for(i=0;i<4;i++){
+                printf("Saisir le nom du joueur %d :\n", i+1);
+                scanf(" %s", repertoire[i].nom);
+            }
+            //Match 1
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, 0, 1);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, 0, 1));
+            
+            //Match 2
+            
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, 2, 3);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, 2, 3));
+            
+            //Match Final
+            
+            /* On trie le tableau en fonction du nombre de points de tournoi */
+            int i, j=0;
+            int gagnants[10] = {0};
+            for(i=0; i<4; i++){
+                    if(repertoire[i].pointsTournoi == 1) {
+                        gagnants[j] = i;
+                        j++;
+                    }
+                }
+            system("cls");
+            printf("Finale : %s VS %s\n\n", repertoire[gagnants[0]].nom, repertoire[gagnants[1]].nom);
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, gagnants[0], gagnants[1]);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, gagnants[0], gagnants[1]));
+            char car;
+            for(i=0; i<4; i++){
+                if(repertoire[i].pointsTournoi == 2){
+                    repertoire[i].score += 10;
+                    printf("10 points ont etes ajoutes au score de %s pour avoir remporte le tournoi !\n", repertoire[i].nom);
+                    printf("\n* Saisir un caractere pour continuer. *\n");
+                    scanf(" %c", &car);
+                    system("cls");
+                }
+            }
+            break;
+        case 8 :
+            initPointsTournoi(repertoire);
+            for(i=0;i<8;i++){
+                printf("Saisir le nom du joueur %d :\n", i+1);
+                scanf(" %s", repertoire[i].nom);
+            }
+            int k = 0;
+            for(j=0,k=1;j<7;j=j+2,k=k+2){
+                initTab(tableau);
+                afficherTab(tableau);
+                do
+                {
+                    modifTabTournoi(tableau, repertoire, i, j, k);
+                    i++;
+                }
+                while(!testGagnantTournoi(tableau, repertoire, j, k));
+            }
+            
+            /* On trie le tableau en fonction du nombre de points de tournoi */
+            for(i=0, j=0; i<8; i++){
+                if(repertoire[i].pointsTournoi == 1) {
+                    gagnants[j] = i;
+                    j++;
+                }
+            }
+            system("cls");
+            printf("Demi-Finale 1 : %s VS %s\n\n", repertoire[gagnants[0]].nom, repertoire[gagnants[1]].nom);
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, gagnants[0], gagnants[1]);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, gagnants[0], gagnants[1]));
+            system("cls");
+            printf("Demi-Finale 2 : %s VS %s\n\n", repertoire[gagnants[2]].nom, repertoire[gagnants[3]].nom);
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, gagnants[2], gagnants[3]);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, gagnants[2], gagnants[3]));
+            
+            /* On trie le tableau en fonction du nombre de points de tournoi */
+            for(i=0, j=0; i<8; i++){
+                if(repertoire[i].pointsTournoi == 2) {
+                    gagnants[j] = i;
+                    j++;
+                }
+            }
+            
+            system("cls");
+            printf("Finale : %s VS %s\n\n", repertoire[gagnants[0]].nom, repertoire[gagnants[1]].nom);
+            initTab(tableau);
+            afficherTab(tableau);
+            do
+            {
+                modifTabTournoi(tableau, repertoire, i, gagnants[0], gagnants[1]);
+                i++;
+            }
+            while(!testGagnantTournoi(tableau, repertoire, gagnants[0], gagnants[1]));
+            for(i=0, j=0; i<8; i++){
+                if(repertoire[i].pointsTournoi == 3){
+                    repertoire[i].score += 20;
+                    printf("20 points ont etes ajoutes au score de %s pour avoir remporte le tournoi !\n", repertoire[i].nom);
+                    printf("\n* Saisir un caractere pour continuer. *\n");
+                    scanf(" %c", &car);
+                    system("cls");
+                }
+            }
+            break;
+        default:
+            printf("Une erreur est survenue !\n");
+            break;
+    }
 }
